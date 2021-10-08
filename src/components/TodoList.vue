8b529243448c83be15b3698811e181c59285ebb6
wushuyang <1565330830@qq.com>
2021-10-08 21:13:01 +0800

    内容抽离成组件

---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="todo-list">
+    <todo-item></todo-item>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem.vue";
+export default {
+  name: "TodoList",
+  components: { TodoItem },
+};
+</script>
+
+<style>
+.todo-list {
+  display: grid;
+  row-gap: 14px;
+}
+</style>
