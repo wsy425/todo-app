733c0fb57ac782c43b7e01a014ce19985259d42c
wushuyang <1565330830@qq.com>
2021-10-08 20:52:59 +0800

    todo-app HTML与CSS部分

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main>
+    <div class="container">
+      <h1>欢迎使用wsy待办事项</h1>
+
+      <div class="add">
+        <input type="text" />
+        <button>
+          <i class="plus"></i>
+        </button>
+      </div>
+
+      <div class="filters">
+        <span class="filter active">全部</span>
+        <span class="filter">已完成</span>
+        <span class="filter">未完成</span>
+      </div>
+
+      <div class="todo-list">
+        <div class="todo-item">
+          <label>
+            <input type="checkbox" />
+            todo内容
+            <span class="check-button"></span>
+          </label>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "./components/HelloWorld.vue";
 
 @Options({
   components: {
@@ -16,12 +42,139 @@ export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Helvetica, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+main {
+  width: 100vw;
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  background: rgb(203, 210, 240);
+}
+
+.container {
+  width: 60%;
+  max-width: 400px;
+  box-shadow: 0px 0px 24px rgba(0, 0, 0, 0.15);
+  border-radius: 24px;
+  padding: 48px 28px;
+  background-color: rgb(245, 246, 252);
+}
+
+h1 {
+  margin: 24px 0;
+  font-size: 28px;
+  color: #414873;
+}
+
+.add {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+.add input {
+  padding: 16px 52px 16px 18px;
+  border-radius: 48px;
+  border: none;
+  outline: none;
+  box-shadow: 0px 0px 24px rgba(0, 0, 0, 0.08);
+  width: 100%;
+  font-size: 16px;
+  color: #626262;
+}
+
+.add button {
+  width: 46px;
+  height: 46px;
+  border-radius: 50%;
+  background: linear-gradient(#c0a5f3, #7f95f7);
+  border: none;
+  outline: none;
+  color: white;
+  position: absolute;
+  right: 0px;
+  cursor: pointer;
+}
+
+.add .plus {
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(#fff, #fff), linear-gradient(#fff, #fff);
+  background-size: 50% 2px, 2px 50%;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.filters {
+  display: flex;
+  margin: 24px 2px;
+  color: #c0c2ce;
+  font-size: 14px;
+}
+
+.filters .filter {
+  margin-right: 14px;
+  transition: 0.8s;
+}
+
+.filters .filter.active {
+  color: #6b729c;
+  transform: scale(1.2);
+}
+
+.todo-list {
+  display: grid;
+  row-gap: 14px;
+}
+
+.todo-item {
+  background: white;
+  padding: 16px;
+  border-radius: 8px;
+  color: #626262;
+}
+
+.todo-item label {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+.todo-item label span.check-button {
+  position: absolute;
+  top: 0;
+}
+
+.todo-item label span.check-button::before,
+.todo-item label span.check-button::after {
+  content: "";
+  display: block;
+  position: absolute;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+}
+.todo-item label span.check-button::before {
+  border: 1px solid #b382f9;
+}
+.todo-item label span.check-button::after {
+  transition: 0.4s;
+  background: #b382f9;
+  transform: translate(1px, 1px) scale(0.8);
+  opacity: 0;
+}
+.todo-item input {
+  margin-right: 16px;
+  opacity: 0;
+}
+.todo-item input:checked + span.check-button::after {
+  opacity: 1;
 }
 </style>
